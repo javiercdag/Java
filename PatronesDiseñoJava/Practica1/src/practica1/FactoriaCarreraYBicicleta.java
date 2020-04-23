04317799a7f22ba1fdc0fc6fc609fb1c90f9c434
javiercdag <58362922+javiercdag@users.noreply.github.com>
2020-04-23 19:54:16 +0200

    Delete FactoriaCarreraYBicicleta.java

--- PatronesDiseñoJava/Practica1/src/practica1/FactoriaCarreraYBicicleta.java
+++
@@ -1,8 +0,0 @@
-package practica1;
-
-public interface FactoriaCarreraYBicicleta {
-
-	public Carrera crearCarrera(int numBicicletas);
-	public Bicicleta crearBicicleta(int numBicicleta, boolean retirarse);
-
-}
